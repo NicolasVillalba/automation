a522291d814dc929b00bdfdfb7458aaa6d3f7ebd
Nicolas Villalba <nerynicolas@gmail.com>
2018-08-13 08:37:08 -0300

    added some lines using Element Locators

--- src/test/java/org/informatorio/FirstTest.java
+++ src/test/java/org/informatorio/FirstTest.java
@@ -1,6 +1,8 @@
 package org.informatorio;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -12,6 +14,9 @@ public class FirstTest {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://the-internet.herokuapp.com/login");
 		System.out.print("Page opened");
+		WebElement username = driver.findElement(By.id("username"));
+		WebElement password = driver.findElement(By.cssSelector("input"));
+		WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
 		driver.quit();
 	}
 }
